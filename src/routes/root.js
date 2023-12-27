e611c8984c3202503ca5c8abb122797ed9e97e90
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2023-12-27 22:48:40 +0530

    things settled for further work

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const authRoute = require("./auth.routes.js");
-// const { createUploadthingExpressHandler } = require("uploadthing/express");
-// const uploadRouter = require("../uploadthing.js");
 const uploadRoute = require("./upload.routes.js");
 
 const rootRoute = express.Router();
--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -1,31 +1,9 @@
 const express = require("express");
-const passport = require("passport");
-const { login } = require("../controllers/auth.controller");
 
-const router = express.Router();
+const uploadRouter = express.Router();
 
-router.get("/login/success", login);
-
-router.get("/login/failed", (req, res) => {
-  res.status(401).json({
-    error: true,
-    message: "Log in failure",
-  });
+uploadRouter.get("/video", (req, res) => {
+  res.send("HELLO");
 });
 
-router.get("/google", passport.authenticate("google", ["profile", "email"]));
-
-router.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    successRedirect: process.env.CLIENT_URL,
-    failureRedirect: "/login/failed",
-  })
-);
-
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect(process.env.CLIENT_URL);
-});
-
-module.exports = router;
+module.exports = uploadRouter;
