90702613d6d480a132c4c2f9e040facb92e2a1cb
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2023-12-28 18:00:40 +0530

    seperating into controllers

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,73 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
+const { uploadOnCloudinary } = require("../utils/cloudinary");
+
+const customizeUser = asyncHandler(async (req, res) => {
+  const { name, user_handle, links } = req.body;
+
+  // Try to update the basic info first
+  let updatedUser = await User.findByIdAndUpdate(
+    req.params.id,
+
+    {
+      $set: { name, user_handle, links },
+    },
+
+    { new: true } // This option returns the modified document, rather than the original
+  ).lean();
+  console.log(updatedUser);
+
+  // Upload Images to Cloudinary
+  console.log(req.files);
+
+  const user_picturePath = req.files?.user_picture
+    ? req.files?.user_picture[0]?.path
+    : null;
+  const banner_imagePath = req.files?.banner_image
+    ? req.files?.banner_image[0]?.path
+    : null;
+
+  const uploadedUserPic = user_picturePath
+    ? await uploadOnCloudinary(user_picturePath)
+    : null;
+
+  const uploadedBannerPic = banner_imagePath
+    ? await uploadOnCloudinary(banner_imagePath)
+    : null;
+
+  if (uploadedUserPic?.url || uploadedBannerPic?.url) {
+    updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+
+      {
+        $set: {
+          ...(uploadedUserPic?.url && { picture: uploadedUserPic.url }),
+          ...(uploadedBannerPic?.url && {
+            banner_image: uploadedBannerPic.url,
+          }),
+        },
+      },
+
+      { new: true } // This option returns the modified document, rather than the original
+    ).lean();
+  }
+
+  console.log(updatedUser);
+
+  res.status(200).json({
+    updatedUser,
+    msg: "Successfully Customized",
+  });
+});
+
+const getUserById = asyncHandler(async (req, res) => {
+  try {
+    const user = User.findById(req.params.id);
+    if (!user) return res.status(404).json({ msg: "User not found" });
+    res.status(200).json({ user, msg: "User Fetched Successfully" });
+  } catch (error) {
+    res.status(500).json({ error, msg: "Internal Server Error" });
+  }
+});
+
+module.exports = { customizeUser, getUserById };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const upload = require("../middlewares/multer.middlewares");
-const User = require("../models/User");
-const { uploadOnCloudinary } = require("../utils/cloudinary");
-const expressAsyncHandler = require("express-async-handler");
+
+const {
+  customizeUser,
+  getUserById,
+} = require("../controllers/user.controller");
 
 const userRouter = express.Router();
 
-userRouter.get("/:id", (req, res) => {
-  res.send(req.user);
-});
+userRouter.get("/:id", getUserById);
 
 userRouter.post(
   "/:id/customize",
@@ -16,63 +16,7 @@ userRouter.post(
     { name: "user_picture", maxCount: 1 },
     { name: "banner_image", maxCount: 1 },
   ]),
-  expressAsyncHandler(async (req, res) => {
-    const { name, user_handle, links } = req.body;
-
-    // Try to update the basic info first
-    let updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-
-      {
-        $set: { name, user_handle, links },
-      },
-
-      { new: true } // This option returns the modified document, rather than the original
-    ).lean();
-    console.log(updatedUser);
-
-    // Upload Images to Cloudinary
-    console.log(req.files);
-
-    const user_picturePath = req.files?.user_picture
-      ? req.files?.user_picture[0]?.path
-      : null;
-    const banner_imagePath = req.files?.banner_image
-      ? req.files?.banner_image[0]?.path
-      : null;
-
-    const uploadedUserPic = user_picturePath
-      ? await uploadOnCloudinary(user_picturePath)
-      : null;
-
-    const uploadedBannerPic = banner_imagePath
-      ? await uploadOnCloudinary(banner_imagePath)
-      : null;
-
-    if (uploadedUserPic?.url || uploadedBannerPic?.url) {
-      updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-
-        {
-          $set: {
-            ...(uploadedUserPic?.url && { picture: uploadedUserPic.url }),
-            ...(uploadedBannerPic?.url && {
-              banner_image: uploadedBannerPic.url,
-            }),
-          },
-        },
-
-        { new: true } // This option returns the modified document, rather than the original
-      ).lean();
-    }
-
-    console.log(updatedUser);
-
-    res.status(200).json({
-      updatedUser,
-      msg: "Successfully Customized",
-    });
-  })
+  customizeUser
 );
 
 module.exports = userRouter;
