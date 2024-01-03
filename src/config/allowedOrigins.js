f010e78bb67bfe126547d53892f631960569507b
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2024-01-03 16:19:16 +0530

    origin added

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,2 +1,5 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://watchify-client.vercel.app",
+];
 module.exports = allowedOrigins;
