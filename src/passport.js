edc0e936767542df4a0443e4b3edbf31ef82d4c7
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2024-01-03 17:23:36 +0530

    removed callback url

--- src/passport.js
+++ src/passport.js
@@ -6,7 +6,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      // callbackURL: "/api/auth/google/callback",
       scope: ["profile", "email"],
     },
     function (accessToken, refreshToken, profile, callback) {
