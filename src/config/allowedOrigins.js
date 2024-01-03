0a79f48f613eb3c1e78b02998a98e880462705d5
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2024-01-03 17:15:57 +0530

    client_url changed

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
   "http://localhost:5173",
   "https://watchify-client.vercel.app",
+  "https://uv-watchify.netlify.app",
+  process.env.CLIENT_URL,
 ];
 module.exports = allowedOrigins;
