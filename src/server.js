de22897e5bd687f9d2b9175524238dd284391110
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2024-01-04 12:50:44 +0530

    cookie config changed again

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const cookieSession = require("cookie-session");
+// const cookieSession = require("cookie-session");
 const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
@@ -8,36 +8,46 @@ const mongoose = require("mongoose");
 const connectToDB = require("./config/connectToDB");
 const corsOptions = require("./config/corsOptions");
 const rootRoute = require("./routes/root");
+
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
 const app = express();
 const port = process.env.PORT || 8080;
 
-require("./passport");
-
 connectToDB();
 
-console.log(("IN_DEVELOPMENT: ", process.env.IN_DEVELOPMENT === "YES"));
-console.log({
-  sameSite: "lax",
-  secure: process.env.IN_DEVELOPMENT !== "YES",
-  httpOnly: process.env.IN_DEVELOPMENT !== "YES",
-  domain: process.env.DOMAIN || "", // Use domain in production
-});
+// console.log(("IN_DEVELOPMENT: ", process.env.IN_DEVELOPMENT === "YES"));
+// console.log({
+//   sameSite: "lax",
+//   secure: process.env.IN_DEVELOPMENT !== "YES",
+//   httpOnly: process.env.IN_DEVELOPMENT !== "YES",
+//   domain: process.env.DOMAIN || "", // Use domain in production
+// });
 
-app.use(
-  cookieSession({
-    name: "session",
-    keys: ["uv_codes"],
-    maxAge: 24 * 60 * 60 * 100 * 7, // 7 Days
-    // sameSite: process.env.IN_DEVELOPMENT !== "YES" ? "none" : "lax",
+// app.use(
+//   cookieSession({
+//     name: "session",
+//     keys: ["uv_codes"],
+//     maxAge: 24 * 60 * 60 * 100 * 7, // 7 Days
+//     // sameSite: process.env.IN_DEVELOPMENT !== "YES" ? "none" : "lax",
 
-    sameSite: "lax",
-    secure: process.env.IN_DEVELOPMENT !== "YES", // Use secure in production
-    httpOnly: process.env.IN_DEVELOPMENT !== "YES", // Use httpOnly in production
-    domain: process.env.DOMAIN || "",
-  })
-);
+//     sameSite: "lax",
+//     secure: process.env.IN_DEVELOPMENT !== "YES", // Use secure in production
+//     httpOnly: process.env.IN_DEVELOPMENT !== "YES", // Use httpOnly in production
+//     domain: process.env.DOMAIN || "",
+//   })
+// );
 
 app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "your_secret_key",
+    resave: false, // Avoid unnecessary session store writes
+    saveUninitialized: false, // Avoid storing uninitialized sessions
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
@@ -45,12 +55,14 @@ app.use(express.static("public"));
 
 // app.use(cookieParser());
 
+require("./passport");
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get("/", (req, res) =>
-  res.status(200).json({ msg: "Welcome to Watchify API" })
-);
+app.get("/", (req, res) => {
+  console.log({ passport_user: req?.session.passport?.user });
+  res.status(200).json({ msg: "Welcome to Watchify API" });
+});
 
 // Routes
 app.use("/api", rootRoute);
