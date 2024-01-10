4481baa4342cf3bb69a52100b4cd43d7863ed917
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2024-01-10 18:34:31 +0530

    create uploads folder automatically if doesn't exist

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,8 +1,16 @@
 const multer = require("multer");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/uploads");
+    const uploadDir = "./public/uploads";
+
+    // Create the 'uploads' directory if it doesn't exist
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
